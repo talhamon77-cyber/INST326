a60342aba7e3bcaf75f868ce6d0ba8a7dadf0c70
talhamon77-cyber <talhamon77@gmail.com>
2025-09-14 22:34:42 -0400

    Update ai-journal.py

--- Library/ai-journal.py
+++ Library/ai-journal.py
@@ -1,1 +1,12 @@
 print('hello world!')
+
+
+AI Usage Journal
+Talha Muhammad 
+Section 0303 
+
+Week 1 
+This week since it was basic python coding I tried my hardest to not use AI to debug or code at all. 
+I ended up being successful and completed the assignment within a couple hours. Since I just finished
+moving I don't have access to a gym besides the one on campus I used Chatgpt to schedule a workout for me to use
+throughout the week. Here is the chat log: https://chatgpt.com/c/68c777db-1cec-8323-bd31-a30e127d4c9e
